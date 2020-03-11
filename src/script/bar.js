691771347d6cb1e79fb14e3a99ed668428996207
Dao Tung <duytung95nb@gmail.com>
2020-03-11 23:05:27 +0700

    March 11, 2020 - Add fonts and xml loader

--- src/script/bar.js
+++ src/script/bar.js
@@ -1,5 +1,8 @@
-import text from '../assets/values/textFile.txt'; 
+import text from '../assets/values/textFile.txt';
+import data from '../assets/values/data.xml';
+
 export default function bar() {
     console.log("Bar runs");
     console.log("Text in text file", text);
+    console.log("Data in xml file", data);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -39,13 +39,25 @@ module.exports = {
     module: {
         rules: [
             { test: /\.txt$/, use: 'raw-loader' },
-            { test: /\.css$/, use: ['style-loader', 'css-loader',] },
+            { test: /\.css$/, use: ['style-loader', 'css-loader', ] },
             {
                 test: /\.(png|svg|jpg|gif)$/,
                 loader: 'file-loader',
                 options: {
                     outputPath: 'assets/images',
                 },
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader',
+                ],
             },
         ],
     },
