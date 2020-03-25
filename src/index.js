f2e82d9e5d18f483f6f36cd9c24fda2db9c7e190
duytung95nb <duytung95nb@gmail.com>
2020-03-25 09:30:31 +0700

    March 25, 2020 - Live reload for source code and serve source by webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import bar from './script/bar';
 import addIcon from './script/addIcon';
+import printMe from './script/print';
 import _ from 'lodash';
 import './assets/style/style.css';
 bar();
+printMe();
 function component() {
     var element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
--- webpack.config.js
+++ webpack.config.js
@@ -3,43 +3,81 @@ const TerserJSPlugin = require('terser-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    entry: './src/index.js', // this one is default
+    entry: {
+        app: './src/index.js', // this one is default
+        print: './src/script/print.js',
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js', // default one
+        filename: '[name].[contenthash].js', // default one
         publicPath: 'http://localhost:8080/',
     },
-    optimization: {
-        minimizer: [new TerserJSPlugin({
-            // terserOptions: {
-            //     output: {
-            //         comments: false,
-            //     },
-            // },
-            minify: (file, sourceMap) => {
-                // https://github.com/mishoo/UglifyJS2#minify-options
-                var uglifyJsOptions = {
-                    /* your `uglify-js` package options */
-                };
-
-                if (sourceMap) {
-                    uglifyJsOptions.sourceMap = {
-                        content: sourceMap,
-                    };
-                }
-
-                return require('uglify-js').minify(file, uglifyJsOptions);
-            },
-        }), new OptimizeCSSAssetsPlugin({})],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
     },
+    // optimization: {
+    //     runtimeChunk: 'single',
+    //     splitChunks: {
+    //         chunks: 'async',
+    //         minSize: 30000,
+    //         minRemainingSize: 0,
+    //         maxSize: 0,
+    //         minChunks: 1,
+    //         maxAsyncRequests: 6,
+    //         maxInitialRequests: 4,
+    //         automaticNameDelimiter: '~',
+    //         automaticNameMaxLength: 30,
+    //         cacheGroups: {
+    //             defaultVendors: {
+    //                 test: /[\\/]node_modules[\\/]/,
+    //                 priority: -10
+    //             },
+    //             default: {
+    //                 minChunks: 2,
+    //                 priority: -20,
+    //                 reuseExistingChunk: true
+    //             }
+    //         }
+    //     },
+    //     minimizer: [new TerserJSPlugin({
+    //         // terserOptions: {
+    //         //     output: {
+    //         //         comments: false,
+    //         //     },
+    //         // },
+    //         minify: (file, sourceMap) => {
+    //             // https://github.com/mishoo/UglifyJS2#minify-options
+    //             var uglifyJsOptions = {
+    //                 /* your `uglify-js` package options */
+    //             };
+
+    //             if (sourceMap) {
+    //                 uglifyJsOptions.sourceMap = {
+    //                     content: sourceMap,
+    //                 };
+    //             }
+
+    //             return require('uglify-js').minify(file, uglifyJsOptions);
+    //         },
+    //     }), new OptimizeCSSAssetsPlugin({})],
+    // },
     // Transform all txt file to valid module (maybe to javascript file) by raw loader
     module: {
-        rules: [
-            { test: /\.txt$/, use: 'raw-loader' },
-            { test: /\.css$/, use: ['style-loader', 'css-loader', ] },
+        rules: [{
+                test: /\.txt$/,
+                use: 'raw-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', ]
+            },
             {
                 test: /\.(png|svg|jpg|gif)$/,
                 loader: 'file-loader',
@@ -62,12 +100,16 @@ module.exports = {
         ],
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: './src/index.html' }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Caching'
+        }),
         new MiniCssExtractPlugin({
             filename: devMode ? '[name].css' : '[name].[hash].css',
             chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
         }),
     ],
     mode: "production",
+    // mode: "development",
     // mode: "none",
 }
