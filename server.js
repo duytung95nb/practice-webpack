f935291dbfa22bd1c060ea76a25a8d6b1d16b0bd
Dao Tung <tung.dao@spiraledge.com>
2020-04-16 22:58:48 +0700

    April 16, 2020 - Add environment variable, support compression when serving file
    Add babel loader to support ES6 syntax

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const webpack = require('webpack');
+const compression = require('compression');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 
 const app = express();
@@ -7,7 +8,18 @@ const config = require('./webpack.config.js');
 const compiler = webpack(config);
 
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
+// configuration file as a base.const shouldCompress = (req, res) => {
+ 
+// const shouldCompress = (req, res) => {
+//   if (req.headers['x-no-compression']) {
+//     // don't compress responses if this request header is present
+//     return false;
+//   }
+
+//   // fallback to standard compression
+//   return compression.filter(req, res);
+// };
+app.use(compression());
 app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath,
 }));
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 // const Uglify = require("uglifyjs-webpack-plugin");
 // const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const {
@@ -9,115 +10,120 @@ const {
 } = require('clean-webpack-plugin');
 const devMode = process.env.NODE_ENV !== 'production';
 
-module.exports = {
-    entry: [
-        path.resolve(__dirname, "src/index.js"), // this one is default
-    ],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
-        publicPath: '/',
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-    },
-    optimization: {
-        runtimeChunk: 'single',
-        moduleIds: 'hashed',
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                },
-            },
-        },
-        minimize: true,
-        minimizer: [new TerserJSPlugin({
-            terserOptions: {
-                keep_classnames: false,
-                mangle: true,
-                compress: false,
-                keep_fnames: false,
-                parse: {
-                    ecma: 8
-                },
-                output: {
-                    comments: false,
-                    beautify: false
-                }
-            },
-            minify: (file, sourceMap) => {
-                const extractedComments = [];
-
-                // Custom logic for extract comments
-
-                const { error, map, code, warnings } = require('uglify-js') // Or require('./path/to/uglify-module')
-                    .minify(file, {
-                        /* Your options for minification */
-                    });
-
-                return { error, map, code, warnings, extractedComments };
-            },
-        }),
+module.exports = env => {
+    // Use env.<YOUR VARIABLE> here:
+    console.log('NODE_ENV: ', env.NODE_ENV); // 'local'
+    console.log('Production: ', env.production); // true
+    return {
+        entry: [
+            path.resolve(__dirname, "src/index.js"), // this one is default
         ],
-    },
-    // Transform all txt file to valid module (maybe to javascript file) by raw loader
-    module: {
-        rules: [{
-            test: /\.txt$/,
-            use: 'raw-loader'
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].[contenthash].js',
+            publicPath: '/',
         },
-        {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader',]
+        devtool: 'inline-source-map',
+        devServer: {
+            contentBase: './dist',
         },
-        {
-            test: /\.(png|svg|jpg|gif)$/,
-            loader: 'file-loader',
-            options: {
-                outputPath: 'assets/images',
+        optimization: {
+            runtimeChunk: 'single',
+            moduleIds: 'hashed',
+            splitChunks: {
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'vendors',
+                        chunks: 'all',
+                    },
+                },
             },
-        },
-        {
-            test: /\.(woff|woff2|eot|ttf|otf)$/,
-            use: [
-                'file-loader',
+            minimize: true,
+            minimizer: [new TerserJSPlugin({
+                terserOptions: {
+                    keep_classnames: false,
+                    mangle: true,
+                    compress: false,
+                    keep_fnames: false,
+                    parse: {
+                        ecma: 8
+                    },
+                    output: {
+                        comments: false,
+                        beautify: false,
+                    }
+                },
+                minify: (file, sourceMap) => {
+                    const extractedComments = [];
+
+                    // Custom logic for extract comments
+
+                    const { error, map, code, warnings } = require('uglify-js') // Or require('./path/to/uglify-module')
+                        .minify(file, {
+                            /* Your options for minification */
+                        });
+
+                    return { error, map, code, warnings, extractedComments };
+                },
+            }),
             ],
         },
-        {
-            test: /\.xml$/,
-            use: [
-                'xml-loader',
-            ],
-        },
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
+        // Transform all txt file to valid module (maybe to javascript file) by raw loader
+        module: {
+            rules: [{
+                test: /\.txt$/,
+                use: 'raw-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader',]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                loader: 'file-loader',
                 options: {
-                    presets: ["@babel/preset-env"]  //Preset used for env setup
-                }
+                    outputPath: 'assets/images',
+                },
             },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env"]  //Preset used for env setup
+                    }
+                },
 
-        }
+            }
+            ],
+        },
+        plugins: [
+            new CompressionPlugin(),
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                title: 'Caching'
+            }),
+            new MiniCssExtractPlugin({
+                filename: devMode ? '[name].css' : '[name].[hash].css',
+                chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+            }),
         ],
-    },
-    plugins: [
-        // new Uglify(),
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Caching'
-        }),
-        new MiniCssExtractPlugin({
-            filename: devMode ? '[name].css' : '[name].[hash].css',
-            chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-        }),
-    ],
-    mode: "production",
-    // mode: "development",
-    // mode: "none",
+        mode: "production",
+        // mode: "development",
+        // mode: "none",
+    }
 }
