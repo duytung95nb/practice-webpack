724590045f63b99bfbb726d40817cf287747d67c
Dao Tung <tung.dao@spiraledge.com>
2020-04-18 16:02:42 +0700

    April 18, 2020 - Divide config files to correspond enviroments

---
+++ webpack.common.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: [
+        path.resolve(__dirname, "src/index.js"),
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        publicPath: '/',
+    },
+    // Transform all txt file to valid module (maybe to javascript file) by raw loader
+    module: {
+        rules: [{
+            test: /\.txt$/,
+            use: 'raw-loader'
+        },
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader',]
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            loader: 'file-loader',
+            options: {
+                outputPath: 'assets/images',
+            },
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: [
+                'file-loader',
+            ],
+        },
+        {
+            test: /\.xml$/,
+            use: [
+                'xml-loader',
+            ],
+        },
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ["@babel/preset-env"]  //Preset used for env setup
+                }
+            },
+
+        }
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Caching'
+        }),
+    ]
+}
---
+++ webpack.prod.js
@@ -0,0 +1,63 @@
+const TerserJSPlugin = require('terser-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+module.exports = merge(common,
+    {
+        mode: "production",
+        devtool: 'source-map',
+        devServer: {
+            contentBase: './dist',
+        },
+        optimization: {
+            runtimeChunk: 'single',
+            moduleIds: 'hashed',
+            splitChunks: {
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'vendors',
+                        chunks: 'all',
+                    },
+                },
+            },
+            minimize: true,
+            minimizer: [new TerserJSPlugin({
+                terserOptions: {
+                    keep_classnames: false,
+                    mangle: true,
+                    compress: false,
+                    keep_fnames: false,
+                    parse: {
+                        ecma: 8
+                    },
+                    output: {
+                        comments: false,
+                        beautify: false,
+                    }
+                },
+                minify: (file, sourceMap) => {
+                    const extractedComments = [];
+
+                    // Custom logic for extract comments
+
+                    const { error, map, code, warnings } = require('uglify-js') // Or require('./path/to/uglify-module')
+                        .minify(file, {
+                            /* Your options for minification */
+                        });
+
+                    return { error, map, code, warnings, extractedComments };
+                },
+            }),
+            ],
+        },
+        plugins: [
+            new CompressionPlugin(),
+            new MiniCssExtractPlugin({
+                filename: '[name].[hash].css',
+                chunkFilename: '[id].[hash].css',
+            }),
+        ],
+    })
+
